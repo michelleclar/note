aa006d7d03bbc9429d94274b65ca17dc8ddbe69c
Carl <carlmichelle493@gmail.com>
2024-07-01 11:52:23 +0800

    add jooq code gen task

--- jooq/src/test/java/org/carl/TestJooq.java
+++ jooq/src/test/java/org/carl/TestJooq.java
@@ -1,7 +1,12 @@
 package org.carl;
 
+import org.carl.jooq.generator.JooqCodeGenerator;
 import org.junit.jupiter.api.Test;
 
 public class TestJooq {
-  
+    @Test
+    public void testGen() throws Exception {
+        JooqCodeGenerator jooqCodeGenerator = new JooqCodeGenerator();
+        jooqCodeGenerator.codeGenByContainers();
+    }
 }
