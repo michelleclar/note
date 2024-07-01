75927306caa5cf5b926ead164daa4786f1ab7991
Carl <carlmichelle493@gmail.com>
2024-07-01 16:27:47 +0800

    fix set cookie

--- application/src/main/java/org/carl/auth/AuthResource.java
+++ application/src/main/java/org/carl/auth/AuthResource.java
@@ -8,35 +8,42 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.NewCookie;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import org.carl.commons.Fields;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.Date;
-
-import org.carl.commons.Fields;
 
 @Path("/oauth")
 public class AuthResource {
 
-  @Inject
-  AuthService authService;
+    @Inject
+    AuthService authService;
+    @ConfigProperty(name = "web.host")
+    String host;
+    @ConfigProperty(name = "web.port")
+    Integer port;
 
-  @GET
-  @Path("github")
-  @PermitAll
-  @Blocking
-  public Uni<Response> github(@Context HttpServerRequest request, @QueryParam("code") String code) {
-    return Uni.createFrom().nullItem().onItem().transform(i -> {
-      String uuid = authService.github(request, code);
-      if (uuid == null) {
-        return Response.seeOther(UriBuilder.fromUri("/").host("localhost").port(3000).build())
-            .build();
-      }
-      NewCookie _cookie = new NewCookie.Builder(Fields.CODE).value(uuid).path("/")
-          .domain("localhost").maxAge(60 * 5).expiry(new Date(System.currentTimeMillis() + 30000))
-          .sameSite(NewCookie.SameSite.NONE).httpOnly(true).secure(true).build();
-      return Response
-          .seeOther(UriBuilder.fromUri("/documents").host("localhost").port(3000).build())
-          .cookie(_cookie).build();
-    });
-  }
+    @GET
+    @Path("github")
+    @PermitAll
+    @Blocking
+    public Uni<Response> github(@Context HttpServerRequest request, @QueryParam("code") String code) {
+        return Uni.createFrom().nullItem().onItem().transform(i -> {
+            String uuid = authService.github(request, code);
+            if (uuid == null) {
+                return Response.seeOther(UriBuilder.fromUri("/").host(host).port(port).build())
+                        .build();
+            }
+            NewCookie _cookie = new NewCookie.Builder(Fields.CODE).value(uuid).path("/")
+                    .domain("localhost").maxAge(60 * 5).expiry(new Date(System.currentTimeMillis() + 30000))
+                    .sameSite(NewCookie.SameSite.NONE).httpOnly(true).secure(true).build();
+            return Response
+                    .seeOther(UriBuilder.fromUri("/documents").host(host).port(port).build())
+                    .cookie(_cookie).build();
+        });
+    }
 }
