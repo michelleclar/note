b813867e663c5ffbd42f69c98ac1508b62091d5b
Carl <carlmichelle493@gmail.com>
2024-07-10 18:03:38 +0800

    remove ui

--- ui/note/app/services/comment-api.ts
+++
@@ -1,2 +0,0 @@
-const host = process.env.NEXT_PUBLIC_HOST;
-export const CHECK_HEALTH = host + "/health/check";
--- ui/note/app/utils/fileds.ts
+++
@@ -1,2 +0,0 @@
-export const REFRESH_TOKEN: string = "refreshToken";
-export const DOCUMENTS: string = "documents";
