b89ffcff56c6c3f15f3f3578e30cd4250ba5994b
Carl <carlmichelle493@gmail.com>
2024-08-09 17:15:01 +0800

    update getsql api

--- jooq/src/main/java/org/carl/jooq/engine/DBEngine.java
+++ jooq/src/main/java/org/carl/jooq/engine/DBEngine.java
@@ -1,24 +1,17 @@
 package org.carl.jooq.engine;
 
 import java.util.function.Function;
+import org.jooq.CloseableQuery;
 import org.jooq.DSLContext;
-import org.jooq.InsertResultStep;
-import org.jooq.Record;
 import org.jooq.SQLDialect;
-import org.jooq.SelectWhereStep;
 import org.jooq.impl.DSL;
 
 public class DBEngine {
     static DSLContext dsl = DSL.using(SQLDialect.POSTGRES);
 
-    public static <R extends Record> String getSqlByJooq(
-        Function<DSLContext, SelectWhereStep<R>> query) {
+    public static String getSqlByJooq(
+        Function<DSLContext, CloseableQuery> query) {
         return query.apply(dsl).getSQL();
     }
 
-    public static String _getSqlByJooq(
-        Function<DSLContext, InsertResultStep<Record>> query) {
-
-        return query.apply(dsl).getSQL();
-    }
 }
